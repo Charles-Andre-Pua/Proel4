f689605c4b50b57577ace9368b5fc25d89df39c8
ACT-STUDENT <ACT-STUDENT@LAB4-PC44>
2025-03-25 09:53:13 +0800

    Add project files.

---
+++ Pua_UserManagement/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pua_UserManagement.Context;
+using Pua_UserManagement.Models;
+
+namespace Pua_UserManagement.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly MyDBContext _dBContext;
+
+        public UserController(MyDBContext dBContext)
+        {
+            _dBContext = dBContext;
+        }
+        public IActionResult Index()
+        {
+            List<Person> person = _dBContext.People.Include(p => p.User).ToList();
+            return View(person);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
