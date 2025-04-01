cf761dee38234ad375832399b9b62d6cdda9f1fa
ACT-STUDENT <ACT-STUDENT@LAB7-PC19>
2025-04-02 07:54:32 +0800

    a

--- Pua_UserManagement/Controllers/UserController.cs
+++ Pua_UserManagement/Controllers/UserController.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Pua_UserManagement.Context;
 using Pua_UserManagement.Models;
 
-namespace Pua_UserManagement.Controllers
+namespace Pua_UserManagement.Controllers    
 {
     public class UserController : Controller
     {
@@ -21,6 +21,20 @@ namespace Pua_UserManagement.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+        public IActionResult CreateProcess(Person person)
+        {
+            if (!ModelState.IsValid) return View(person);
+            person.DateCreated = DateTime.Now;
+
+            _dBContext.People.Add(person);
+            _dBContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult UpdatePartial(int id)
+        {
+            Person? person = _dBContext.People.Include(a=>a.User).FirstOrDefault(p => p.Id == id);
+            return PartialView();
         }
     }
 }
